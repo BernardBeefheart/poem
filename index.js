62376d93bfd787e845c76c5341d05121c673dc17
bernard <bernard.beefheart@outlook.fr>
2015-01-28 23:09:32 +0100

    première version du routeur

--- index.js
+++ index.js
@@ -3,5 +3,6 @@
  */
 
 var server = require("./server");
+var router = require("./router");
 
-server.start();
+server.start(router.route);
--- server.js
+++ server.js
@@ -3,16 +3,21 @@
  */
 
 var http = require("http");
+var url = require("url");
+var port = 8888;
 
-function start() {
+
+function start(route) {
   function onRequest(request, response) {
-    console.log("Request received.");
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+	route(pathname);
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
     response.end();
   }
 
-  http.createServer(onRequest).listen(8888);
+  http.createServer(onRequest).listen(port);
   console.log("Server has started.");
 }
 
